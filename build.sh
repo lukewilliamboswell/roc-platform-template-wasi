71a0bbaac03ff245b495b8940307892ce8d0f201
Luke Boswell <lukewilliamboswell@gmail.com>
2024-08-03 19:03:30 +1000

    improve build script

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
-# let's manually build and link the roc app and host together
+#!/bin/bash
+
+# script to manually prebuild the host
+# built the app and then link them together
 
 # step 1. build the prebuilt-host
 zig build
 
-# step 1a. copy into our platform directory (where roc can find it if this was packaged into a URL)
+# step 1a. copy into our platform directory
+# (where it lives in a URL package)
 cp zig-out/lib/libhost.a platform/wasm32.a
 
-#  step 2. build the roc app (normally done internally by roc)
-roc build --target=wasm32 --no-link --emit-llvm-ir app.roc
+#  step 2. build the roc app
+#  (normally done internally by roc)
+roc build \
+    --target=wasm32 \
+    --no-link \
+    --emit-llvm-ir \
+    app.roc
 
-#  step 3. link the app and host together (normally done internally by roc)
-zig build-exe -target wasm32-wasi-musl -lc -O ReleaseSmall -fPIC -fstrip platform/wasm32.a app.ll -femit-bin=out.wasm
-
-#  step 4. check the output
-ls -hl out.wasm
+#  step 3. link the app and host together
+#  (normally done internally by roc)
+zig build-exe \
+    -target wasm32-wasi-musl \
+    -lc -O ReleaseSmall \
+    -fPIC \
+    -fstrip \
+    -femit-bin=out.wasm \
+    platform/wasm32.a \
+    app.ll
